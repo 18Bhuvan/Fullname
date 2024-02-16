dec2f278e73b27faff862b08799b12d4ddfa68eb
bhuvan <bhuvan@BHUVAN>
2024-02-17 00:37:00 +0530

    First commit

---
+++ src/main/java/Name.java
@@ -0,0 +1,41 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Name
+ */
+@WebServlet("/Name")
+public class Name extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Name() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String fname=request.getParameter("fname");
+		String lname=request.getParameter("lname");
+		String fullname=fname+lname;
+		out.print(fullname);
+	}
+
+	
+
+}
